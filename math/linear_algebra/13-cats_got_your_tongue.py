a67d56dc1094839fd1a239047d2379208c24ae6d
Amaël Lounkokobi <lounkokobia@gmail.com>
2023-12-07 12:39:05 +0000

    Exercises 13

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import numpy as np
+
+"""
+This module contains a function that concatenates
+two matrices along a specific axis
+
+Examples:
+   >>> mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+   ... mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+   ... print(np_cat(mat1, mat2))
+   ... print(np_cat(mat1, mat2, axis=1))
+   [[11 22 33]
+    [44 55 66]
+    [ 1  2  3]
+    [ 4  5  6]]
+   [[11 22 33  1  2  3]
+    [44 55 66  4  5  6]]
+
+Functions:
+   def np_cat(mat1, mat2, axis=0)
+
+Libraries:
+   Numpy
+
+"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    This function def np_cat(mat1, mat2, axis=0):
+    concatenates two matrices along a specific axis.
+
+    Args:
+       mat1: Numpy n dimension matrix
+       mat2: Numpy n dimension matrix
+
+    Returns
+       new_matrix: the new concatenated matrix
+
+    """
+    new_matrix = np.concatenate((mat1, mat2), axis)
+
+    return new_matrix
