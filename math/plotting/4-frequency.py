b70ed41b25927f574fbe2b6423ee0af51c23b565
Amaël Lounkokobi <lounkokobia@gmail.com>
2023-12-09 18:23:24 +0000

    Exercise 4

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+bins = np.arange(0,101,10)
+plt.hist(student_grades,bins,ec="k")
+plt.xticks(bins)
+plt.xlabel("Number of students")
+plt.ylabel("Grades")
+plt.xlim(0,100)
+plt.ylim(0,30)
+plt.title("Project A")
+plt.show()
