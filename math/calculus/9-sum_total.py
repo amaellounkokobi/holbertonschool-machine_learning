539b519635edefc56786b5a92c189ef89d48af70
Amaël Lounkokobi <lounkokobia@gmail.com>
2023-12-13 09:46:10 +0000

    Exercise 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module contains:
+- A function calculates the sum  from i = 1 to n of i^2
+
+Example:
+   >>> n = 5
+   ... print(summation_i_squared(n))
+   55
+
+Function:
+   def summation_i_squared(n):
+
+"""
+
+
+def summation_i_squared(n):
+    """
+    This function calculates the sum of i^2
+    from i = 1 to n equals 0
+
+    Args:
+       n(int):An integer superior to 1
+    """
+    nums = [*range(1, n+1)]
+    pow_nums = list(map(lambda num : pow(num, 2), nums))
+    result = sum(pow_nums)
+
+    return result
