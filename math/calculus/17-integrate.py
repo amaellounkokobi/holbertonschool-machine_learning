84880c209881ffa5d59a8e53d868d9d034a6ff39
Amaël Lounkokobi <lounkokobia@gmail.com>
2023-12-14 15:36:27 +0000

    Exercise 17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+This module contains:
+- A function that calculates the integral of a polynomial
+
+Example:
+   >>> poly = [5, 3, 0, 1]
+   ... print(poly_integral(poly))
+   [0, 5, 1.5, 0, 0.25]
+
+Function:
+   def poly_integral(poly, C=0):
+
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+    This function that calculates the integral of a polynomial
+
+    Args:
+       poly(list): the list represents the power of x that the coefficient
+       c: integer representing the integration constant
+    """
+
+    if type(poly) is not list:
+        return None
+    if len(poly) < 1:
+        return None
+    if type(C) is not int:
+        return None
+
+    for index in range(len(poly)):
+        if type(poly[index]) is not int:
+            return None       
+        coef = poly[index]
+        if (index + 1) > 0:
+            poly[index] = coef * (1 /(index + 1))
+
+    poly.insert(0,C)
+    
+    return poly
