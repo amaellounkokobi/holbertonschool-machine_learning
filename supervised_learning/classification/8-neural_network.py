174f5a5388aaf2375bfe1fc728191f780b2f63d3
Amaël Lounkokobi <lounkokobia@gmail.com>
2024-01-09 20:23:15 +0000

    Exercise 8

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -51,11 +51,11 @@ class NeuralNetwork():
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
 
-        self.__W1 = np.random.normal(size=(1, nx))
-        self.__b1 = np.zeros((1, nx))
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
 
-        self.__W2 = np.random.normal(size=(1, nx))
+        self.__W2 = np.random.normal(size=(1, nodes))
         self.__b2 = 0
         self.__A2 = 0
 
@@ -106,3 +106,51 @@ class NeuralNetwork():
         """
 
         return self.__A2
+
+    @W1.setter
+    def W1(self, value):
+        """
+        setting the hidden layer weight vector
+        """
+
+        self.__W1 = value
+
+    @W2.setter
+    def W2(self, value):
+        """
+        setting the output layer weight vector
+        """
+
+        self.__W2 = value
+
+    @b1.setter
+    def b1(self, value):
+        """
+        setting the hidden layer bias vector
+        """
+
+        self.__b1 = value
+
+    @b2.setter
+    def b2(self, value):
+        """
+        setting the output layer bias
+        """
+
+        self.__b2 = value
+
+    @A1.setter
+    def A1(self, value):
+        """
+        setting the hidden layer activation
+        """
+
+        self.__A1 = value
+
+    @b2.setter
+    def A2(self, value):
+        """
+        setting the output layer activation
+        """
+
+        self.__A2 = value
