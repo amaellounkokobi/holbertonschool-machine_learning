c75a8623b7d7784f8b33ba236265b200999cec73
Amaël Lounkokobi <lounkokobia@gmail.com>
2024-01-10 15:56:52 +0000

    Exercises 7

--- supervised_learning/classification/7-neuron.py
+++ supervised_learning/classification/7-neuron.py
@@ -166,33 +166,7 @@ class Neuron():
             return 1
         else:
             return 0
-
-    def gradient_descent(self, X, Y, A, alpha=0.05):
-        """
-        Calculates one pass of gradient descent on the neuron
-        Updates the private attributes __W and __b
-
-        Args:
-        X: is a numpy.ndarray with shape (nx, m) that contains
-        the input data nx is the number of input features to the neuron
-        m is the number of examples
-
-        Y: is a numpy.ndarray with shape (1, m) that contains
-        the correct labels for the input data
-
-        A: is a numpy.ndarray with shape (1, m) containing the
-        activated output of the neuron for each example alpha
-        is the learning rate
-
-        """
-        N = X.shape[1]
-
-        dW = 1 / N * np.dot((A - Y), X.T)
-        db = 1 / N * np.sum((A - Y))
-
-        self.__W = self.__W - alpha * dW
-        self.__b = self.__b - alpha * db
-
+        
     def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True, graph=True, step=100):
         """
         Trains the neuron
