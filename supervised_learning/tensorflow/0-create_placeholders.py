da22c869069afef4b6ded856ae25ffae4438ea18
Amaël Lounkokobi <lounkokobia@gmail.com>
2024-01-15 21:04:43 +0000

    Exo 0

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,29 @@
+#!/usr/bin/ennv python3
+"""
+This module contains:
+a function that returns two placeholders, x and y, for the neural network:
+
+Function:
+   def create_placeholders(nx, classes):
+
+"""
+
+
+def create_placeholders(nx, classes):
+    """
+    Function that returns two placeholders, x and y, for the neural network
+
+    Args:
+       nx: the number of feature columns in our data
+
+       classes: the number of classes in our classifier
+
+       Returns:
+        placeholders named x and y, respectively
+        x is the placeholder for the input data to the neural network
+        y is the placeholder for the one-hot labels for the input data
+    """
+        x = tf.placeholder(tf.float32, [1, nx])
+        y = tf.placeholder(tf.float32, [1, classes])
+
+        return x, y
