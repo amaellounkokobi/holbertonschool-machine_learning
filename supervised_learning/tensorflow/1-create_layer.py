6f6cd316c451e26d80c1ddfef41bf9a3a3983121
amael <lounkokobia@gmail.com>
2024-01-17 23:32:12 +0000

    Exo1

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
 This module contains:
-a function that returns two placeholders, x and y, for the neural network:
-
+a function that creates a layer
 Function:
    def create_placeholders(nx, classes):
 
@@ -12,29 +11,24 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """
-    Function that returns two placeholders, x and y, for the neural network
+    Function that creates a layer
 
     Args:
-       nx: the number of feature columns in our data
-
-       classes: the number of classes in our classifier
-
-       Returns:
-        placeholders named x and y, respectively
-        x is the placeholder for the input data to the neural network
-        y is the placeholder for the one-hot labels for the input data
+      prev: is the tensor output of the previous layer
+      n: is the number of nodes in the layer to create
+      activation: is the activation function that the layer should use
+      Returns: the tensor output of the layer
     """
 
     w_init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     name = activation.__name__.capitalize()
 
-    layer = tf.layers.dense(
-            prev,
+    layer = tf.layers.Dense(
             n,
             activation=activation,
             kernel_initializer=w_init,
             bias_initializer=tf.compat.v1.zeros_initializer(),
-            name=name,
+            name="layer",
     )
 
-    return layer
+    return layer(prev)
