a5f8367117edac83c6d8819e30b9e61e21b89004
Amael <lounkokobia@gmail.com>
2024-01-18 13:30:15 +0100

    Exo 3

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+This module contains:
+a function that creates that calculates the accuracy of a prediction:
+Function:
+   def create_placeholders(nx, classes):
+
+"""
+import tensorflow.compat.v1 as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    Function that  that calculates the accuracy of a prediction
+
+    Args:
+       y is a placeholder for the labels of the input data
+       y_pred is a tensor containing the network’s predictions
+       Returns: a tensor containing the decimal accuracy of the prediction
+       hint: accuracy = correct_predictions / all_predictions
+
+    """
+
+    pred = tf.equal(tf.argmax(y), tf.argmax(y_pred))
+    accuracy = tf.reduce_mean(tf.cast(pred, tf.float32))
+
+    return accuracy
