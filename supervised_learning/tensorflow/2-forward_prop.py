a9dbd895d25c69a0d6c567c6689ff5ea31e63b9d
amael <lounkokobia@gmail.com>
2024-01-18 00:02:50 +0000

    Exo2

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
 This module contains:
-A function that creates the forward propagation graph for the neural network:
+A function that creates the forward propagation graph 
+for the neural network:
 Function:
    def forward_prop(x, layer_sizes=[], activations=[]):
 """
 import tensorflow.compat.v1 as tf
 create_layer = __import__('1-create_layer').create_layer
+
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
@@ -14,11 +16,16 @@ def forward_prop(x, layer_sizes=[], activations=[]):
 
     Args:
         x: is the placeholder for the input data
-        layer_sizes: is a list containing the number of nodes in each layer of the network
-        activations: is a list containing the activation functions for each layer of the network
+
+        layer_sizes: is a list containing the number of nodes in each 
+        layer of the network
+
+        activations: is a list containing the activation functions for 
+        each layer of the network
+
         Returns: the prediction of the network in tensor form
     """
-    prev = x 
+    prev = x
     for nodes, activation in zip(layer_sizes, activations):
         prev = create_layer(prev, nodes, activation)
 
