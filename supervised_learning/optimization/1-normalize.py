70e8f172fff1911cb6a503d5279b57ea95994315
Amael <lounkokobia@gmail.com>
2024-01-19 10:33:00 +0100

    Exo 1

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -15,15 +15,21 @@ def normalize(X, m, s):
 
     Args:
        X: is the numpy.ndarray of shape (d, nx) to normalize
+
        d: is the number of data points
+
        nx: is the number of features
-       m: is a numpy.ndarray of shape (nx,) that contains the mean of all features of X
-       s: is a numpy.ndarray of shape (nx,) that contains the standard deviation of all features of X
+
+       m: is a numpy.ndarray of shape (nx,)
+       that contains the mean of all features of X
+
+       s: is a numpy.ndarray of shape (nx,) that contains the
+       standard deviation of all features of X
 
     Returns:
        The normalized X matrix
 
     """
-    norm_matrix = np.divide(np.subtract(X, m),np.sqrt(s**2 + 0.0000000001))
+    norm_matrix = np.divide(np.subtract(X, m), np.sqrt(s**2 + 0.0000000001))
 
     return norm_matrix
