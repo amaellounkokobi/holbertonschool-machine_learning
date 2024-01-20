c1f79d05cf2bd3acd44c272276e8b02b642c664c
Amael <lounkokobia@gmail.com>
2024-01-20 23:22:46 +0100

    Exo 7

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This module contains
+A function that updates a variable using the RMSProp
+optimization algorithm
+
+Function:
+   def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+"""
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    Function that updates a variable using the RMSProp
+    optimization algorithm
+
+    Args:
+       alpha: is the learning rate
+       beta2: is the RMSProp weight
+       epsilon: is a small number to avoid division by zero
+       var: is a numpy.ndarray containing the variable to be updated
+       grad: is a numpy.ndarray containing the gradient of var
+       s: is the previous second moment of var
+    Returns:
+       the updated variable and the new moment, respectively
+    """
+
+    sdX = beta2 * s + (1 - beta2) * grad**2
+    var = var - alpha * (grad / np.sqrt(sdX + epsilon))
+
+    return var, sdX
