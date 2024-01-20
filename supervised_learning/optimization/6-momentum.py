3a88b1e4749c26115887d3efbd4d0a32df112e71
Amael <lounkokobia@gmail.com>
2024-01-20 22:38:43 +0100

    Exo 6

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This module contains
+A function that creates the training operation for a neural network
+in tensorflow using the gradient descent with momentum
+optimization algorithm:
+
+Function:
+  def create_momentum_op(loss, alpha, beta1):
+"""
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    function that creates the training operation for
+    a neural network in tensorflow using the gradient
+    descent with momentum optimization algorithm:
+
+    Args:
+       loss: is the loss of the network
+       alpha: is the learning rate
+       beta1: is the momentum weight
+    Returns:
+       the momentum optimization operation
+
+    """
+
+    op = tf.compat.v1.train.MomentumOptimizer(alpha).minimize(loss)
+
+    return op
