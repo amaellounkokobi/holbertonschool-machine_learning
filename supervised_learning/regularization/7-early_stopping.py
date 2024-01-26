22871341c1cfb4df735d515383200ba8402b3469
Amael <lounkokobia@gmail.com>
2024-01-26 09:59:52 +0100

    Exo 7

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3                                                                                                                                                                                      
+"""                                                                                                                                                                                                         
+This module contains :                                                                                                                                                                                      
+A function that determines if you should stop gradient descent early                                                                                                                                        
+Function:                                                                                                                                                                                                   
+   def early_stopping(cost, opt_cost, threshold, patience, count):                                                                                                                                          
+"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """                                                                                                                                                                                                        Function that determines if you should stop gradient descent early                                                                                                                                     
+
+    Args:                                                                                                                                                                                                   
+       cost: is the current validation cost of the neural network                                                                                                                                                 opt_cost: is the lowest recorded validation cost of the neural network                                                                                                                                     threshold: is the threshold used for early stopping                                                                                                                                                        patience: is the patience count used for early stopping                                                                                                                                                    count: is the count of how long the threshold has not been met
+
+    Returns:                                                                                                                                                                                                       a boolean of whether the network should be stopped                                                                                                                                                         early, followed by the updated count                                                                                                                                                                    """
+    
+    if count + 1 >= patience:
+        return True, count + 1
+    else:
+        return False, count + 1 
