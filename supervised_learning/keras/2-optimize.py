afe7e40ebf2fffa0e3b025987edb66e1c60050a2
Amael <lounkokobia@gmail.com>
2024-01-30 11:40:53 +0100

    Exo 2

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+This module contains :
+a function that sets up Adam optimization for a keras
+ model with categorical crossentropy loss and accuracy metrics
+
+Function:
+   def optimize_model(network, alpha, beta1, beta2):
+"""
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Function that sets up Adam optimization for a keras
+    model with categorical crossentropy loss and accuracy metrics
+
+    Args:
+       network: is the model to optimize
+       alpha: is the learning rate
+       beta1: is the first Adam optimization parameter
+       beta2: is the second Adam optimization parameter
+    Returns:
+       None
+    """
+    opt = keras.optimizers.Adam(learning_rate=alpha,
+                                beta_1=beta1,
+                                beta_2=beta2)
+
+    model.compile(loss='categorical_crossentropy', optimizer=opt)
+
+    return None
