4d827b0390f642b4e0b1743d058b0cdb2e91fb51
Amael <lounkokobia@gmail.com>
2024-01-30 10:42:04 +0100

    Exo 1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -3,43 +3,40 @@
 This module contains :
 A function that builds a neural network with the Keras library
 
-
 Function:
    def build_model(nx, layers, activations, lambtha, keep_prob):
 """
-import numpy as np
-import tensorflow as tf
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     Function that builds a neural network with the Keras library
-    
+
     Args:
        nx: is the number of input features to the network
-    
-       layers: is a list containing the number of nodes 
+
+       layers: is a list containing the number of nodes
        in each layer of the network
-    
-       activations: is a list containing the activation 
+
+       activations: is a list containing the activation
        functions used for each layer of the network
-    
+
        lambtha: is the L2 regularization parameter
-    
-       keep_prob: is the probability that a node will be 
+
+       keep_prob: is the probability that a node will be
        kept for dropout
 
-    Returns: 
+    Returns:
        the keras model
     """
     inputs = K.Input(shape=(nx,))
     K.regularizers.L2(l2=lambtha)
-    
+
     L = len(layers)
     x = inputs
 
-    for layer_unit, activation in zip(layers[:-1], activations[:-1]):                                                                                                                   
+    for layer_unit, activation in zip(layers[:-1], activations[:-1]):
         x = K.layers.Dense(layer_unit,
                            kernel_regularizer="L2",
                            activation=activation)(x)
@@ -52,5 +49,5 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         activation=activations[L - 1])(x)
 
     model = K.Model(inputs=inputs, outputs=outputs)
-    
+
     return model
