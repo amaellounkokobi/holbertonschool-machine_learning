d9b4fab1b3455c559f6bb82fc094770e591985fc
Amael <lounkokobia@gmail.com>
2024-01-30 14:38:58 +0100

    Exo 4 pep8

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -5,7 +5,7 @@ This module contains :
 A function that trains a model using mini-batch gradient descent
 
 Function:
-   def train_model(network, 
+   def train_model(network,
                    data,
                    labels,
                    batch_size,
@@ -25,10 +25,10 @@ def train_model(network,
                 shuffle=False):
     """
     Function that trains a model using mini-batch gradient descent
-    
+
     Args:
        network: is the model to train
-       
+
        data: is a numpy.ndarray of shape (m, nx)
        containing the input data
 
@@ -38,20 +38,20 @@ def train_model(network,
        batch_size: is the size of the batch used for
        mini-batch gradient descent
 
-       epochs: is the number of passes through data for mini-batch gradient descent
-       
+       epochs: is the number of passes through data for
+       mini-batch gradient descent
+
        verbose: is a boolean that determines if output
        should be printed during training
-       
-       shuffle: is a boolean that determines whether to shuffle 
-       the batches every epoch. Normally, it is a good idea 
+
+       shuffle: is a boolean that determines whether to shuffle
+       the batches every epoch. Normally, it is a good idea
        to shuffle, but for reproducibility, we have chosen
        to set the default to False.
-    
+
     Returns:
        the History object generated after training the model
     """
-
     history = network.fit(
         x=data,
         y=labels,
