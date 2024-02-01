5c6addc39ca3d3776b800725e6f7db939c35fdae
Amael <lounkokobia@gmail.com>
2024-02-01 23:34:23 +0100

    Exo 1

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -2,7 +2,6 @@
 """
 This module contains :
 A function that performs a same convolution on grayscale images
-
 Function:
    def convolve_grayscale_same(images, kernel):
 """
@@ -12,19 +11,16 @@ import numpy as np
 def convolve_grayscale_same(images, kernel):
     """
     Function that performs a same convolution on grayscale images:
-
     Args:
     images: is a numpy.ndarray with shape (m, h, w)
     containing multiple grayscale images
        m is the number of images
        h is the height in pixels of the images
        w is the width in pixels of the images
-
     kernel: is a numpy.ndarray with shape (kh, kw)
     containing the kernel for the convolution
        kh is the height of the kernel
        kw is the width of the kernel
-
     Returns:
        A numpy.ndarray containing the convolved images
     """
@@ -50,23 +46,27 @@ def convolve_grayscale_same(images, kernel):
     pad_H = np.max((out_h - 1) * Sh + Fh - H, 0)
     pad_W = np.max((out_w - 1) * Sw + Fw - W, 0)
 
+    
     pad_top = pad_H // 2
     pad_bottom = pad_H - pad_top
     pad_left = pad_W // 2
     pad_right = pad_W - pad_left
 
+    
     imgs_H = H + pad_H
     imgs_W = W + pad_W
+
     images_pad = np.zeros((m, imgs_H, imgs_W))
+   
     images_pad[:, pad_top:-pad_bottom, pad_left:-pad_right] = images
     # Initialize output images
-
+   
     grayscaled_imgs = np.zeros(shape=(m, out_h, out_w))
-
+    
     # Perform the convolution on all images
     for y in range(out_h):
         for x in range(out_w):
-            filter_img = images_pad[:, y:Fh + y, x:Fw + x]
+            filter_img = images_pad[:, y * Sh:y * Sh + Fh, x * Sw:x * Sw + Fw]
             op = np.sum(filter_img * kernel, axis=1)
             convoluted = np.sum(op, axis=1, keepdims=True)
             # add operations in all layer images
