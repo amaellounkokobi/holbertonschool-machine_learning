cd31a3f4ec030c424ca4816bc9387d2445c8fba2
Amael <lounkokobia@gmail.com>
2024-02-05 09:58:14 +0100

    Exo 6

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that builds,
+trains, and saves a neural network classifier
+
+
+Function:
+   def train(X_train,
+             Y_train,
+             X_valid,
+             Y_valid,
+             layer_sizes,
+             activations,
+             alpha,
+             iterations,
+             save_path="/tmp/model.ckpt"):
+"""
+import tensorflow.compat.v1 as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(X_train,
+          Y_train,
+          X_valid,
+          Y_valid,
+          layer_sizes,
+          activations,
+          alpha,
+          iterations,
+          save_path="/tmp/model.ckpt"):
+    """
+    Function that builds, trains, and saves a neural network classifier
+
+    Args:
+       X_train: is a numpy.ndarray containing the training input data
+       Y_train: is a numpy.ndarray containing the training labels
+       X_valid: is a numpy.ndarray containing the validation input data
+       Y_valid: is a numpy.ndarray containing the validation labels
+       layer_sizes: is a list containing the number of nodes in
+       each layer of the network
+       activations: is a list containing the activation functions
+       for each layer of the network
+       alpha: is the learning rate
+       iterations: is the number of iterations to train over
+       save_path: designates where to save the model
+
+    """
+    x, y = create_placeholders(784,10)
+    y_pred = forward_prop(x, [256, 256,10], [tf.nn.tanh, tf.nn.tanh, None])
+    accuracy = calculate_accuracy(y, y_pred)
+    loss = calculate_loss(y, y_pred)
+    train_op = create_train_op(loss, 0.01)
+    saver = tf.train.Saver()
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        train_cost = sess.run(
+            loss, feed_dict={x: X_train, y: Y_train})
+        train_accuracy = sess.run(
+            accuracy, feed_dict={x: X_train, y: Y_train})
+        valid_cost = sess.run(
+            loss, feed_dict={x: X_valid, y: Y_valid})
+        valid_accuracy = sess.run(
+            accuracy, feed_dict={x: X_valid, y: Y_valid})
+
+        print("After {} epochs:".format(0))
+        print("\tTraining Cost: {} ".format(train_cost))
+        print("\tTraining Accuracy: {}".format(train_accuracy))
+        print("\tValidation Cost: {}".format(valid_cost))
+        print("\tValidation Accuracy: {}".format(valid_accuracy))
+
+        for i in range(iterations):
+
+            if i % 100 == 0 and i > 0:
+                train_cost = sess.run(
+                    loss, feed_dict={x: X_train, y: Y_train})
+                train_accuracy = sess.run(
+                    accuracy, feed_dict={x: X_train, y: Y_train})
+                valid_cost = sess.run(
+                    loss, feed_dict={x: X_valid, y: Y_valid})
+                valid_accuracy = sess.run(
+                    accuracy, feed_dict={x: X_valid, y: Y_valid})
+                print("After {} epochs:".format(i))
+                print("\tTraining Cost: {} ".format(train_cost))
+                print("\tTraining Accuracy: {}".format(train_accuracy))
+                print("\tValidation Cost: {}".format(valid_cost))
+                print("\tValidation Accuracy: {}".format(valid_accuracy))
+
+            sess.run(train_op, feed_dict={x:X_train, y:Y_train})
+
+        print("After {} epochs:".format(i + 1))
+        print("\tTraining Cost: {} ".format(train_cost))
+        print("\tTraining Accuracy: {}".format(train_accuracy))
+        print("\tValidation Cost: {}".format(valid_cost))
+        print("\tValidation Accuracy: {}".format(valid_accuracy))
+
+        save_path = saver.save(sess, save_path)
+
+    return save_path
