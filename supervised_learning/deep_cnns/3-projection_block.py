89ffb41cc5015c87a1720d34a1091aec5e323cee
Amael <lounkokobia@gmail.com>
2024-02-14 23:49:28 +0100

    Exo 3

---
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+"""
+This module contains :
+Function that builds a projection block
+
+Function:
+   def projection_block(A_prev, filters, s=2):
+"""
+import tensorflow.keras as K
+
+def projection_block(A_prev, filters, s=2):
+    """
+    Builds a projection block
+    
+    Args:
+    A_prev is the output from the previous layer
+   
+    filters is a tuple or list containing F11, F3, F12, respectively:
+       F11 is the number of filters in the first 1x1 convolution
+       
+       F3 is the number of filters in the 3x3 convolution
+       
+       F12 is the number of filters in the second 1x1 convolution
+       as well as the 1x1 convolution in the shortcut connection
+    
+    s is the stride of the first convolution in both the main 
+    path and the shortcut connection
+    """
+    # Init filters
+    F11, F3, F12 = filters
+
+    # conv1x1 shortcut connection
+    conv1x1_add = K.layers.Conv2D(F12, (1, 1), strides=s)(A_prev)    
+
+    # Batch Norm
+    Bn1_add =K.layers.BatchNormalization()(conv1x1_add)
+
+    # Conv1x1
+    conv1x1 = K.layers.Conv2D(F11,(1, 1), strides=s)(A_prev)
+
+    # Batch Norm
+    Bn1 = K.layers.BatchNormalization()(conv1x1)
+
+    # Relu activation
+    relu_1 = K.layers.ReLU()(Bn1)
+
+    # Conv3x3
+    conv3x3 = K.layers.Conv2D(F3, (3, 3), padding="same")(relu_1)
+
+    # Batch Norm
+    Bn2 =  K.layers.BatchNormalization()(conv3x3)
+
+    # Relu activation
+    relu_2 = K.layers.ReLU()(Bn2)
+    conv1x1_2 = K.layers.Conv2D(F12, (1, 1))(relu_2)
+
+ 
+    # Batch Norm
+    Bn3 =  K.layers.BatchNormalization()(conv1x1_2)
+
+    # Concatenate outputs
+    add = K.layers.Concatenate()([Bn3, Bn1_add])
+
+    # Relu activation
+    relu_3 = K.layers.ReLU()(add)
+
+    return relu_3
