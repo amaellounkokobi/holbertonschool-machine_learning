69ce4b6906b61b428f6712432bd229a8910503e0
Amael <lounkokobia@gmail.com>
2024-02-29 09:29:54 +0100

    Exo 6

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -20,11 +20,18 @@ def transition_layer(X, nb_filters, compression):
        compression is the compression factor for the transition layer
 
     """
+    # Init Kernel
+    init = K.initializers.VarianceScaling(scale=2.0,
+                                          mode='fan_in',
+                                          distribution='truncated_normal',
+                                          seed=None)
+
     # Conv 1x1
     bn = K.layers.BatchNormalization()(X)
     relu = K.layers.ReLU()(bn)
     conv = K.layers.Conv2D(filters=nb_filters * compression,
-                           kernel_size=(1, 1))(relu)
+                           kernel_size=(1, 1),
+                           kernel_initializer=init)(relu)
 
     # Average pooling 2x2 stride 2
     avg_pool = K.layers.AveragePooling2D(pool_size=(2, 2),
