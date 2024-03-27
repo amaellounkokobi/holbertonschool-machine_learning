10034758fd6e03f97bb4aff6fda4358e8d405259
Amael <lounkokobia@gmail.com>
2024-03-27 18:17:27 +0100

    exo 0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
 This module contains :
-A function that calculates the likelihood 
-of obtaining this data given various 
+A function that calculates the likelihood
+of obtaining this data given various
 hypothetical probabilities of developing severe side effects
 
 Function:
@@ -10,18 +10,20 @@ def likelihood(x, n, P):
 """
 import numpy as np
 
+
 def likelihood(x, n, P):
     """
-    function def likelihood(x, n, P): that calculates the likelihood 
-    of obtaining this data given various hypothetical probabilities 
+    function def likelihood(x, n, P): that calculates the likelihood
+    of obtaining this data given various hypothetical probabilities
     of developing severe side effects
 
     """
-    if not isinstance(n, int) and n < 0:
+    if not isinstance(n, int) or n <=  0:
         raise ValueError('n must be a positive integer')
 
-    if not isinstance(x, int) and x >= 0:
-        raise ValueError('x must be an integer that is greater than or equal to 0')
+    if not isinstance(x, int) or x >= 0:
+        raise ValueError
+    ('x must be an integer that is greater than or equal to 0')
 
     if x > n:
         raise ValueError('x cannot be greater than n')
@@ -29,7 +31,7 @@ def likelihood(x, n, P):
     if not isinstance(P,np.ndarray):
         raise TypeError('P must be a 1D numpy.ndarray')
 
-    if not np.any(P > 0)  and np.any(P < 1):
+    if not np.any(P > 0) and np.any(P < 1):
         raise ValueError('All values in P must be in the range')
 
     L = np.math.comb(n, x) * (P ** x) * ((1 - P) ** (n - x))
