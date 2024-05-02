46acca28ff9e8d11d4a81efe83fab71b08fb3154
Amael <lounkokobia@gmail.com>
2024-05-02 09:53:18 +0200

    exo 0

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -12,14 +12,19 @@ import numpy as np
 class RNNCell():
     """
     Rnn Cell
+
+    Attributes:
+      i is the dimensionality of the data
+      h is the dimensionality of the hidden state
+      o is the dimensionality of the outputs
     """
     def __init__(self, i, h, o):
         """
         RNN Init weight and bias
         """
         self.Wh = np.random.randn(h + i, h)
-        self.bh = np.random.randn(h, o)
-        self.Wy = np.zeros((1, h))
+        self.Wy = np.random.randn(h, o)
+        self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
@@ -31,7 +36,7 @@ class RNNCell():
         h_next = np.tanh(np.dot(inputs, self.Wh) + self.bh)
 
         # compute softmax output
-        y = np.dot(h_next, self.Wy.T) + self.by
+        y = np.dot(h_next, self.Wy) + self.by
         y = np.exp(y) / np.sum(np.exp(y), axis=1, keepdims=True)
 
         return h_next, y
